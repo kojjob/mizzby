7393f6a5494099566e26358dda794b6fbe80b267
Kojo <kodeon6@gmail.com>
2025-11-24 23:04:17 +0000

    fix: Register new product page controllers in index.js
    - Registered hero-carousel, sticky-nav, filter-panel, price-range, and featured-carousel controllers
    - Ensures Stimulus controllers are properly initialized
    - Fixes broken layout issues on products page

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,6 +9,11 @@ import CartDropdownController from "./cart_dropdown_controller"
 import ProfilePictureController from "./profile_picture_controller"
 import SellerRegistrationController from "./seller_registration_controller"
 import TabController from "./tab_controller"
+import HeroCarouselController from "./hero_carousel_controller"
+import StickyNavController from "./sticky_nav_controller"
+import FilterPanelController from "./filter_panel_controller"
+import PriceRangeController from "./price_range_controller"
+import FeaturedCarouselController from "./featured_carousel_controller"
 
 // Register controllers
 application.register("dropdown", DropdownController)
@@ -19,6 +24,11 @@ application.register("cart-dropdown", CartDropdownController)
 application.register("profile-picture", ProfilePictureController)
 application.register("seller-registration", SellerRegistrationController)
 application.register("tab", TabController)
+application.register("hero-carousel", HeroCarouselController)
+application.register("sticky-nav", StickyNavController)
+application.register("filter-panel", FilterPanelController)
+application.register("price-range", PriceRangeController)
+application.register("featured-carousel", FeaturedCarouselController)
 
 // Optionally import all controllers from `controllers` directory
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
